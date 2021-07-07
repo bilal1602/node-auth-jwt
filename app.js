69daa0f08c3f7c831c62707cdcbe1bb549584544
Bilal <bilal.sajjad786@gmail.com>
2021-07-07 19:42:49 +0500

    Auth done

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
+const express = require('express');
+const jwt = require('jsonwebtoken');
 
-const app = express()
+const app = express();
 
-app.get('/api', (req,res) => res.json({ message: 'Welcome to API'}))
+app.get('/api', (req, res) => res.json({ message: 'Welcome to API' }));
 
-app.post('/api/post', (req,res) => res.json({message: 'Post Created'}))
+app.post('/api/post', verifyToken, (req, res) => {
+	jwt.verify(req.token, 'secretKey', (err, authData) => {
+		if (err) {
+			res.sendStatus(403);
+		} else {
+			res.json({
+				message: 'Post created...',
+				authData,
+			});
+		}
+	});
+});
 
-app.post('/api/login', verifyToken,(req, res) => {
-    // Mock User
-    const user = {
-        id: 1,
-        username:'Bilal',
-        email: 'bilal.sajjad786@gmail.com'
-    }
-    
-    jwt.sign({user}, 'secretKey',(err, token)=>res.json({token}))
-})
+app.post('/api/login', (req, res) => {
+	// Mock User
+	const user = {
+		id: 1,
+		username: 'Bilal',
+		email: 'bilal.sajjad786@gmail.com',
+	};
+
+	jwt.sign({ user }, 'secretKey', (err, token) => res.json({ token }));
+});
 
 // Verify Token
-function verifyToken(req,res,next){
-    // Get auth header value
-    const bearerHeader = req.header['authorization'];
-    // Check if bearer is undefined
-    if(typeof bearerHeader !== 'undefined') {
+function verifyToken(req, res, next) {
+	// Get auth header value
+	const bearerHeader = req.header['authorization'];
 
-    } else {
-        // Forbidden
-        res.sendStatus()
-    }
+	// Check if bearer is undefined
+	if (typeof bearerHeader !== 'undefined') {
+		const bearer = bearerHeader.split(' ');
+		// Get token from array
+		const bearerToken = bearer[1];
+
+		req.token = bearerToken;
+		next();
+	} else {
+		// Forbidden
+		res.sendStatus(403);
+	}
 }
 
-app.listen(5000, ()=> console.log('Server listening on port 5000'))
+app.listen(5000, () => console.log('Server listening on port 5000'));
