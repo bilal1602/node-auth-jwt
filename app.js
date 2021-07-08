c917826dcd1966f4862e9be475a78d295eb6dc9f
Bilal <bilal.sajjad786@gmail.com>
2021-07-08 16:35:54 +0500

    Feat: Added expiry in jwt.

--- app.js
+++ app.js
@@ -7,39 +7,28 @@ app.get('/api', (req, res) => res.json({ message: 'Welcome to API' }));
 
 app.post('/api/post', verifyToken, (req, res) => {
 	jwt.verify(req.token, 'secretKey', (err, authData) => {
-		if (err) {
-			res.sendStatus(403);
-		} else {
-			res.json({
-				message: 'Post created...',
-				authData,
-			});
-		}
+		if (err) res.sendStatus(403);
+		else res.json({ message: 'Post created...', authData });
 	});
 });
 
 app.post('/api/login', (req, res) => {
 	// Mock User
-	const user = {
-		id: 1,
-		username: 'Bilal',
-		email: 'bilal.sajjad786@gmail.com',
-	};
-
-	jwt.sign({ user }, 'secretKey', (err, token) => res.json({ token }));
+	const user = { id: 1, username: 'user', email: 'test@user.com' };
+	jwt.sign({ user }, 'secretKey', { expiresIn: '30s' }, (err, token) => res.json({ token }));
 });
 
 // Verify Token
 function verifyToken(req, res, next) {
 	// Get auth header value
-	const bearerHeader = req.header['authorization'];
-
+	const bearerHeader = req.headers['authorization'];
+	console.log('bearerHeader: ', bearerHeader);
 	// Check if bearer is undefined
 	if (typeof bearerHeader !== 'undefined') {
 		const bearer = bearerHeader.split(' ');
 		// Get token from array
 		const bearerToken = bearer[1];
-
+		// Add token in request
 		req.token = bearerToken;
 		next();
 	} else {
